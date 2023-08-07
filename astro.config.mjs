6e83dc4e65977faca1f72067adfb5d332d56223f
David <hanktech1.0@gmail.com>
2023-08-06 21:30:08 -0400

    build: :fire: remove config for gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    site: 'https://HankTech.github.io',
-    base: '/HankTech',
-    build: {
-        assets: 'astro'
-    }
-});
+export default defineConfig({});
